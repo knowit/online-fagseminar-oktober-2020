fdb80f7df7d5872b53706d0df4ec106b6ab8cad0
Thomas Malt <thomas@malt.no>
2020-06-15 20:26:55 +0200

    updated index.js to use online fagseminar logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,8 @@ import ContentSection from '../components/ContentSection';
 import spacing from '../util/spacing';
 import mediaQueries from '../util/mediaQueries';
 import NextUp from '../components/NextUp';
-import SundvoldenLogo from '../components/Icons/SundvoldenLogo';
+// import SundvoldenLogo from '../components/Icons/SundvoldenLogo';
+import OnlineLogo from '../components/Icons/OnlineLogo';
 
 const StyledLogoContainer = styled.div`
   text-align: center;
@@ -20,14 +21,15 @@ const StyledLogoContainer = styled.div`
   }
 `;
 
+// <SundvoldenLogo />
 const Frontpage = () => (
   <Layout showGoogleMaps>
     <Content>
       <ContentSection backgroundColor={colors.blueDark} color="white">
         <StyledLogoContainer>
-          <SundvoldenLogo />
+          <OnlineLogo />
           <Paragraph color="white">
-            {eventData.startDate} - {eventData.endDate} {eventData.year}
+            {eventData.startDate}, {eventData.year}
           </Paragraph>
         </StyledLogoContainer>
       </ContentSection>
@@ -39,7 +41,12 @@ const Frontpage = () => (
           Fagseminar {eventData.season.longCapitalized} {eventData.year}
         </h1>
         <Paragraph>
-          <h3>På grunn av Coronavirus-utbruddet er dette fagseminaret utsatt inntil videre.</h3><br/><br/>
+          <h3>
+            På grunn av Coronavirus-utbruddet er dette fagseminaret utsatt
+            inntil videre.
+          </h3>
+          <br />
+          <br />
           {`Knowit Objectnet arrangerer to fagseminarer årlig. Det er de ansatte
           selv som lager programmet. På dette seminaret skulle også noen fra Amende delta. 
           ${eventData.season.longCapitalized} ${eventData.year} skulle det vært avholdt ${eventData.startDate} -
